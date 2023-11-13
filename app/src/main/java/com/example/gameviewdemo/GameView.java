c8f5a485da471cec4351306a91769c530def2bab
andrew-qian <andrewyukaiqian@gmail.com>
2023-11-12 21:17:26 -0600

    small_alien sprite

--- app/src/main/java/com/example/gameviewdemo/GameView.java
+++ app/src/main/java/com/example/gameviewdemo/GameView.java
@@ -9,11 +9,13 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.os.Handler;
 import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.time.chrono.ChronoLocalDate;
+import java.util.Random;
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
@@ -23,6 +25,14 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     Bitmap background;
     Rect rect;
     int dWidth, dHeight;
+
+    Bitmap small_alien[] = new Bitmap[2];
+    int small_alienX, smallAlienY, velocity, small_alienFrame;
+    int small_alienWidth;
+
+    Handler handler;
+    Runnable runnable;
+    final long UPDATE_MILLIS = 30;
 
     public GameView(Context context) {
         super(context);
@@ -37,6 +47,20 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         dHeight= size.y;
         rect = new Rect(0,0,dWidth,dHeight);
         thread = new MainThread(getHolder(), this);
+        small_alien[0] = BitmapFactory.decodeResource(getResources(), R.drawable.small_alien1);
+        small_alien[1] = BitmapFactory.decodeResource(getResources(), R.drawable.small_alien2);
+        small_alienX = 400;
+        smallAlienY = 100;
+        velocity = 2;
+        small_alienFrame = 0;
+        small_alienWidth = small_alien[0].getWidth();
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                invalidate();
+            }
+        };
     }
 
     @Override
@@ -71,17 +95,26 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         characterSprite.update();
     }
 
-//    @Override
-//    protected void onDraw(Canvas canvas){
-//        super.onDraw(canvas);
-//        canvas.drawBitmap(background,null,rect,null);
-//    }
+    @Override
+    protected void onDraw(Canvas canvas){
+
+    }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
         canvas.drawBitmap(background,null,rect,null);
-        characterSprite.draw(canvas);
+        canvas.drawBitmap(small_alien[small_alienFrame], small_alienX, smallAlienY, null);
+
+        if small_alienFrame++;
+        if(small_alienFrame > 1){
+            small_alienFrame = 0;
+        }
+        small_alienX -= velocity;
+        if (small_alienX < 5 || small_alienX > (dWidth - small_alienWidth - 5)){
+            velocity = -velocity;
+        }
+        handler.postDelayed(runnable, UPDATE_MILLIS);
     }
 
 
